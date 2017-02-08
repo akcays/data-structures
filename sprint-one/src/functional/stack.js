b9be6f223903a1667e68ffd47cda793a02f0e96d
akcay <ilkesedefakcay@gmail.com>
2017-02-08 11:16:37 -0800

    Imnplementing stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,18 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var size = 0;
+  
   someInstance.push = function(value) {
+    size++;
   };
 
   someInstance.pop = function() {
+    size--;
   };
 
   someInstance.size = function() {
+    return size < 0 ? 0 : size;
   };
 
   return someInstance;
