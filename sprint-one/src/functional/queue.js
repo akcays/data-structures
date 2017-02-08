36cf3bbd2659d7d11bd52677adcd0292c5a92ce6
akcay <ilkesedefakcay@gmail.com>
2017-02-08 11:28:59 -0800

    Completed queue AND stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,22 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var size = 0;
+  
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    var removed = storage[0];
+    delete storage[0]
+    size--;
+    return removed;
   };
 
   someInstance.size = function() {
+    return size < 0 ? 0 : size;
   };
 
   return someInstance;
