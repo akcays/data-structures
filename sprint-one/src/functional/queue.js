f65da80fca45c740de670913f016f31f743402d1
akcays <ilkesedefakcay@gmail.com>
2017-02-08 15:03:33 -0800

    Completed functional/queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,21 +6,25 @@ var Queue = function() {
 
   // Implement the methods below
   var size = 0;
+  var start = 0;
   
   someInstance.enqueue = function(value) {
-    storage[size] = value;
+    storage[start + size] = value;
     size++;
   };
 
   someInstance.dequeue = function() {
-    var removed = storage[0];
-    delete storage[0]
-    size--;
-    return removed;
+    if (size) {
+      size--;
+      var removed = storage[start];
+      delete storage[start];
+      start++;
+      return removed;
+    }
   };
 
   someInstance.size = function() {
-    return size < 0 ? 0 : size;
+    return size;
   };
 
   return someInstance;
