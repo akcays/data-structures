0596a4016b07bec4c0ec979355b4c999713e31bc
akcays <ilkesedefakcay@gmail.com>
2017-02-09 11:30:22 -0800

    Completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value); //create new node
+    if (list.tail) { //if there's already something in the list
+      list.tail.next = node; //point the last object of list to our new node
+    }
+    if (!list.tail) { //if there's nothing in the list
+      list.head = node; //point the head to our new node
+    }
+    list.tail = node; //rename the new element we just added to tail
   };
 
   list.removeHead = function() {
+    var removed = null;
+    if (list.head === list.tail && list.head) { //if only one element
+      removed = list.head.value;
+      list.head = null;  //delete that element, set head and tail to null
+      list.tail = null;
+    } else { //there is more than 1 element
+      removed = list.head.value; //set removed to equal the head value
+      list.head = list.head.next;  
+    }
+    return removed;
   };
 
   list.contains = function(target) {
+    var curr = list.head;
+    while (curr) {
+      if (curr.value === target) {
+        return true;
+      }
+      curr = curr.next;
+    }
+    return false;
   };
 
   return list;
