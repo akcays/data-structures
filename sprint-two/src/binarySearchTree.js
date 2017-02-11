c7d1cf4008cbcc41b704ef9640599ecf8bbce6f8
akcays <ilkesedefakcay@gmail.com>
2017-02-10 16:01:56 -0800

    Implemented binary search tree / contains

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,56 @@
 var BinarySearchTree = function(value) {
+  
+  var newNode = Object.create(bstMethods);
+  newNode.value = value;
+  newNode.left = null;
+  newNode.right = null;
+  return newNode;
 };
+
+var bstMethods = {};
+
+bstMethods.insert = function(val) {
+  var newNode = BinarySearchTree(val);
+
+  var test = function(node) {
+    if (val < node.value) {
+      if (node.left) {
+        test(node.left);
+      } else {
+        node.left = newNode;
+        return;
+      }
+    } else if (val > node.value) {
+      if (node.right) {
+        test(node.right);
+      } else {
+        node.right = newNode;
+        return;
+      }
+    }
+  };
+
+  test(this);
+};
+
+bstMethods.contains = function(val) {
+  if (this.value === val) {
+    return true;
+  } else {
+    var leftHas = false;
+    var rightHas = false;
+    if (this.left) { 
+      leftHas = this.left.contains(val);
+    }
+    if (this.right) {
+      rightHas = this.right.contains(val);   
+    }
+  }
+
+  return (leftHas || rightHas);
+};
+
+bstMethods.depthFirstLog = function() {};
 
 
 /*
