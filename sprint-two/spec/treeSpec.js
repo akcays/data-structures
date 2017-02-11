edde209347abc74509e30a2f5b41b1ac15b3d13f
akcays <ilkesedefakcay@gmail.com>
2017-02-10 20:26:48 -0800

    Added a remove element function to tree and tested for it in treeSpec

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,4 +41,13 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
+  it('should remove children from the tree', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    tree.removeChild(6);
+    expect(tree.contains(6)).to.equal(false);
+  });
+
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -31,6 +31,23 @@ treeMethods.contains = function(target) {
 
 };
 
+treeMethods.removeChild = function(value, parent) {
+  if (this.value === value) {
+    var cur = this;
+    _.each(parent.children, function(parentsChild, i) {
+      if (cur === parentsChild) {
+        parent.children.splice(i, 1);
+      }
+    });
+  } else {
+    var par = this;
+    _.each(this.children, function(child) {
+      child.removeChild(value, par);
+    });
+  }
+
+};
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
